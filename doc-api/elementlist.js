868ae35676c1056efd2baab16e2fd8e0ac0c1533
Tomasz Zadora <tomek@inovum.pl>
2017-08-08 04:51:27 +0200

    Add files via upload

---
+++ doc-api/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","inopx\\cache\\AdapterInterfaceCacheMethod"],["c","inopx\\cache\\AdapterInterfaceSynchro"],["c","inopx\\cache\\CacheMethodFile"],["c","inopx\\cache\\CacheMethodMemcached"],["c","inopx\\cache\\CacheMethodPDO"],["c","inopx\\cache\\InterfaceCacheMethod"],["c","inopx\\cache\\InterfaceSynchro"],["c","inopx\\io\\IOTool"],["c","PDO"],["c","SyncReaderWriter"]];
