e8c407c6079e8159161aefdee58e772cf56a06b6
root <root@ns346492.ip-91-121-177.eu>
2017-08-08 05:07:47 +0200

    Removed api-doc html

--- doc-api/elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","inopx\\cache\\AdapterInterfaceCacheMethod"],["c","inopx\\cache\\AdapterInterfaceSynchro"],["c","inopx\\cache\\CacheMethodFile"],["c","inopx\\cache\\CacheMethodMemcached"],["c","inopx\\cache\\CacheMethodPDO"],["c","inopx\\cache\\InterfaceCacheMethod"],["c","inopx\\cache\\InterfaceSynchro"],["c","inopx\\io\\IOTool"],["c","PDO"],["c","SyncReaderWriter"]];
