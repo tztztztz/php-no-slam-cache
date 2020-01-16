8ad12d5977c269d63d68d339132a8cd2d1534aff
Tomasz Zadora <tomek@inovum.pl>
2020-01-16 10:28:42 +0100

    Update AdapterInterfaceInputOutput.php

--- vendor/inopx/cache/AdapterInterfaceInputOutput.php
+++ vendor/inopx/cache/AdapterInterfaceInputOutput.php
@@ -8,13 +8,13 @@ namespace inopx\cache;
  */
 class AdapterInterfaceInputOutput implements InterfaceInputOutput {
   
-  public function &input($dataForStorage) {
+  public function input($dataForStorage) {
     
     return \inopx\io\IOTool::dataToBase64($dataForStorage);
     
   }
 
-  public function &output($dataFromStorage) {
+  public function output($dataFromStorage) {
     
     return \inopx\io\IOTool::dataFromBase64($dataFromStorage);
     
